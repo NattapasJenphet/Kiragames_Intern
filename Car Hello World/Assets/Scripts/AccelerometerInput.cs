40fe660f52e51a68d28c0e0d0eb935e73e762ec3
Nattapas <nattapas@kiragames.com>
2017-11-02 16:20:17 +0700

    #3 เพิ่ม script Accelerometer Input

---
+++ Car Hello World/Assets/Scripts/AccelerometerInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AccelerometerInput : MonoBehaviour {
+
+    public int Count = 0;
+
+	void Start () {
+        
+    }
+	
+	void Update () {
+        AccInput();
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed left click.");
+            Count++;
+        }
+        if(Count >= 3)
+        {
+            SceneManager.LoadScene(0);
+            Count = -1;
+        }
+    }
+    void AccInput()
+    {
+        transform.Translate(Input.acceleration.x, 0, 0);
+    }
+}
