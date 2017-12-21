433bba513506d27d6dab20e8c98fcf2ee3ab4b16
Nattapas <nattapas@kiragames.com>
2017-12-21 13:52:11 +0700

    #13 เพิ่m script การกด tap to play แล้วเปลี่ยนไปหน้า game

---
+++ TicTacToe/Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour {
+
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+        Onclick();
+    }
+
+    void Onclick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Game");
+        }
+    }
+}
