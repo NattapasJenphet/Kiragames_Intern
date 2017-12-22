b9d80a5fc5aaedf6df6f528d552e556b4fa96a58
Nattapas <nattapas@kiragames.com>
2017-12-22 13:30:38 +0700

    #13 เพิ่มปุ่ม < ( Back ) ย้อนกลับไปหน้า menu

---
+++ TicTacToe/Assets/Scripts/GameEvnetManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEvnetManager : MonoBehaviour {
+	
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+    public void backtoMenu()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Menu");
+    }
+}
