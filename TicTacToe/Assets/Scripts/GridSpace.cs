2aff6e2f9732a28dbe73211c476462620f8cf11d
Nattapas <nattapas@kiragames.com>
2017-12-27 17:02:16 +0700

    #14 เพิ่มระบบเกม tic tax toe สร้าง script GameController เพื่อควบคุมระบบเกม สร้าง script GridSpace เพื่อควบคุมช่องตาราง

--- TicTacToe/Assets/Scripts/GridSpace.cs
+++ TicTacToe/Assets/Scripts/GridSpace.cs
@@ -1,16 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GridSpace : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Button button;
+    public Text buttonText;
+    public string playerSide;
+    private GameController gameController;
+    public GameObject sc_GameController;
+    
+
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+
+    public void SetSpace()
+    {
+        buttonText.text = gameController.GetPlayerSide();      
+        button.interactable = false;
+        if (sc_GameController.GetComponent<GameController>().playerSide == "O")
+        {
+
+            this.buttonText.GetComponent<Text>().color = new Color(0.83f, 0.27f, 0.39f);
+        }
+        else
+        {
+            this.buttonText.GetComponent<Text>().color = new Color(0.35f, 0.56f, 0.76f);
+        }
+        gameController.EndTurn();  
+    }  
 }
+
